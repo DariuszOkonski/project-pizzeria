dcab7da7e9e18e67f145e6bc2436f980d54d802e
Dariusz Okoński <darek200180@gmail.com>
2025-05-06 10:49:54 +0200

    add functionality to switch between order and booking subpage

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,40 @@
-import Cart from "./components/Cart.js";
-import Product from "./components/Product.js";
-import { select, settings } from "./settings.js";
+import Cart from './components/Cart.js';
+import Product from './components/Product.js';
+import { classNames, select, settings } from './settings.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activatePage(id);
+
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
   initData: function () {
     const thisApp = this;
     thisApp.data = {};
@@ -38,13 +70,30 @@ const app = {
     thisApp.cart = new Cart(cartElem);
 
     thisApp.productList = document.querySelector(select.containerOf.menu);
-    thisApp.productList.addEventListener('add-to-cart', function(event) {
+    thisApp.productList.addEventListener('add-to-cart', function (event) {
       app.cart.add(event.detail.product);
-    })
+    });
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
   },
 
   init: function () {
     const thisApp = this;
+
+    this.initPages();
 
     thisApp.initData();
     thisApp.initCart();
