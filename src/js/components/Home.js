4232fc055ef125e144145a84f6f9defcd5f326de
Dariusz Okoński <darek200180@gmail.com>
2025-05-13 12:53:15 +0200

    add carousel

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -29,10 +29,10 @@ class Home {
 
     var elem = document.querySelector('.main-carousel');
     var flkty = new Flickity(elem, {
-      // options
       cellAlign: 'left',
       contain: true,
-      autoPlay: 1000,
+      autoPlay: 1500,
+      fullscreen: true,
     });
   }
 }
